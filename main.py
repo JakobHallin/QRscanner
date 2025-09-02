23f48717ba491894b5f092cd18e03f8ce65005e9
Jakob Hallin <jakob.l.hallin@gmail.com>
2025-09-02 18:07:14 +0200

    Add QR code byte mode encoding function

--- main.py
+++ main.py
@@ -21,3 +21,30 @@ def decode_byte_mode(data: list[int]) -> str:
     return bytes(data).decode('utf-8')
 encode_byte_mode_result = encode_byte_mode("Hello, World!")
 print(encode_byte_mode_result)  # Output: [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]
+
+print("will try with qr")
+def qr_encode_byte_mode(data: str) -> list[int]: #will only do 1L version
+    """Encode a string to 1-L qr code byte mode."""
+    bitstream = "0100"  # Mode indicator for byte mode //this tells we use byte
+    
+    bitstream += format(len(data), '08b')  # Character count indicator for my (8 bits for version 1 Byte mode)
+
+    for char in data: #we need to convert each char to its byte value so i need to encode each char
+        bitstream += format(ord(char), '08b')  # 8 bits per character we use ord to get the byte value of each char
+    # Add terminator
+    bitstream += "0000"  # Terminator (4 bits)
+    # Pad to make the length a multiple of 8
+    while len(bitstream) % 8 != 0:
+        bitstream += "0" # Pad with 0s so we can get full bytes
+    
+    #  Convert to integers (codewords)
+    codewords = [int(bitstream[i:i+8], 2) for i in range(0, len(bitstream), 8)]
+
+    # Pad with 0xEC and 0x11 to reach the required length for version 1-L (19 codewords)
+    while len(codewords) < 19:
+        if len(codewords) % 2 == 0:
+            codewords.append(0xEC)
+        else:
+            codewords.append(0x11)
+    return codewords
+print(qr_encode_byte_mode("HELLO"))
